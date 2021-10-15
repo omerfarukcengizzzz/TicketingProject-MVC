6e380c2e557e25a90069841c88ff3cdef6db1396
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-15 23:15:11 +0100

    Ticketing Project - MVC

---
+++ src/main/java/com/cybertek/entity/User.java
@@ -0,0 +1,12 @@
+package com.cybertek.entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    private String firstName;
+    private String lastName;
+}
