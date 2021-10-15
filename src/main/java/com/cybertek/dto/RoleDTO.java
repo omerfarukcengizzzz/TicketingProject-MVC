104f66c66e9e6f64e8cdbe36cb7a0c62781e1332
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-15 23:33:16 +0100

    Ticketing Project - MVC

---
+++ src/main/java/com/cybertek/dto/RoleDTO.java
@@ -0,0 +1,6 @@
+package com.cybertek.dto;
+
+public class RoleDTO {
+    private Long id;
+    private String description;
+}
---
+++ src/main/java/com/cybertek/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.cybertek.dto;
+
+import com.cybertek.enums.Gender;
+
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private boolean enabled;
+    private String phone;
+    private RoleDTO role;
+    private Gender gender;
+}
