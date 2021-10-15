5fdb379c1baf0e1eae245b7394d68f87f2c10464
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-15 18:57:57 +0100

    Ticketing Project - MVC

--- src/main/java/com/cybertek/controller/AdminController.java
+++ src/main/java/com/cybertek/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,9 +14,13 @@ public class AdminController {
         return "/admin/project-create";
     }
 
-    @GetMapping("/user-create")
+    @GetMapping({"/user-create", "/user-add", "/user-initialize"})
     public String userCreate() {
         return "/admin/user-create";
     }
 
+    @PostMapping("/user-create/save")
+    public String saveUser() {
+        return "/admin/user-create";
+    }
 }
