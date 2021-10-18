282f53a9c5e5233aa2e45872f4941d7bc9aa4ec4
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-18 16:58:42 +0100

    Ticketing Project - MVC

---
+++ src/main/java/com/cybertek/implementation/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.cybertek.implementation;
+
+import com.cybertek.dto.UserDTO;
+import com.cybertek.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl extends AbstractMapService<UserDTO, String > implements UserService {
+    @Override
+    public UserDTO save(UserDTO obj) {
+        return super.save(obj.getUserName(), obj);
+    }
+
+    @Override
+    public UserDTO findByID(String id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public List<UserDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(String id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public void delete(UserDTO obj) {
+        super.delete(obj);
+    }
+}
