0daf70af4b22860a0a17a385347e60d8a84c8537
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-18 17:32:14 +0100

    Ticketing Project - MVC

---
+++ src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -0,0 +1,31 @@
+package com.cybertek.bootstrap;
+
+import com.cybertek.dto.RoleDTO;
+import com.cybertek.service.RoleService;
+import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataGenerator implements CommandLineRunner {
+
+    @Autowired
+    RoleService roleService;
+    @Autowired
+    UserService userService;
+
+    // Whenever the SpringBoot application will start, it will first run this method
+    @Override
+    public void run(String... args) throws Exception {
+
+        RoleDTO adminRole = new RoleDTO(1L, "Admin");
+        RoleDTO managerRole = new RoleDTO(2L, "Manager");
+        RoleDTO employeeRole = new RoleDTO(3L, "Employee");
+
+        roleService.save(adminRole);
+        roleService.save(managerRole);
+        roleService.save(employeeRole);
+
+    }
+}
