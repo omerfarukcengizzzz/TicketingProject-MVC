74f0d7121423fa3940d99b9de7336a60550a7582
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-18 23:22:00 +0100

    Ticketing Project - MVC

--- src/main/java/com/cybertek/controller/AdminController.java
+++ src/main/java/com/cybertek/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,27 +22,33 @@ public class AdminController {
     RoleService roleService;
 
 
-    // Project Creation
-    @GetMapping("/project-create")
-    public String projectCreate() {
-        return "/admin/project-create";
-    }
-
-
-
     // User Creation
     @GetMapping({"/user-create", "/user-add", "/user-initialize"})
     public String userCreate(Model model) {
 
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roleList", roleService.findAll());
+        model.addAttribute("userList", userService.findAll());
 
         return "/admin/user-create";
     }
 
     @PostMapping("/user-create/save")
-    public String saveUser() {
+    public String saveUser(@ModelAttribute("user") UserDTO userDTO) {
+
+        userService.save(userDTO);
+
         return "/admin/user-create";
+    }
+
+
+
+
+
+    // Project Creation
+    @GetMapping("/project-create")
+    public String projectCreate() {
+        return "/admin/project-create";
     }
 
 }
