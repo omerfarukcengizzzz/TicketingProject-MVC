08a46592b6e1d695631a4d797aedc2746eac56e2
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-18 14:57:07 +0100

    Ticketing Project - MVC

--- src/main/java/com/cybertek/service/UserService.java
+++ src/main/java/com/cybertek/service/UserService.java
@@ -1,4 +1,19 @@
 package com.cybertek.service;
 
+import com.cybertek.dto.UserDTO;
+
+import java.util.List;
+
 public interface UserService {
+    // save user
+    UserDTO save(UserDTO user);
+
+    // find user by username
+    UserDTO findByID(String username);
+
+    // return user list
+    List<UserDTO> findAll();
+
+    // delete user
+    UserDTO delete(UserDTO user);
 }
