af665d1f24c83911b425bf241400fe7fe8592639
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-18 17:45:21 +0100

    Ticketing Project - MVC

--- src/main/java/com/cybertek/controller/AdminController.java
+++ src/main/java/com/cybertek/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.cybertek.controller;
 
 import com.cybertek.dto.UserDTO;
+import com.cybertek.service.RoleService;
+import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +14,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
+    // DI
+    @Autowired
+    UserService userService;
+    @Autowired
+    RoleService roleService;
+
+
     // Project Creation
     @GetMapping("/project-create")
     public String projectCreate() {
         return "/admin/project-create";
     }
 
+
+
     // User Creation
     @GetMapping({"/user-create", "/user-add", "/user-initialize"})
     public String userCreate(Model model) {
 
         model.addAttribute("user", new UserDTO());
+        model.addAttribute("roleList", roleService.findAll());
 
         return "/admin/user-create";
     }
