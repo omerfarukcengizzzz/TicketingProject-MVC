a9771f13083b0dfec89008be45986ca1b2bcdd1c
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-11-13 17:00:59 +0100

    Ticketing Project - MVC

--- src/main/java/com/cybertek/bootstrap/DataGenerator.java
+++ src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -66,9 +66,9 @@ public class DataGenerator implements CommandLineRunner {
         userService.save(user8);
 
         // Pumping Project data objects
-        ProjectDTO project1 = new ProjectDTO("Spring MVC","PR001",user1, LocalDate.now(),LocalDate.now().plusDays(25),"Creating Controllers", Status.OPEN);
-        ProjectDTO project2 = new ProjectDTO("Spring ORM","PR002",user2, LocalDate.now(),LocalDate.now().plusDays(10),"Creating Database", Status.IN_PROGRESS);
-        ProjectDTO project3 = new ProjectDTO("Spring Container","PR003",user1, LocalDate.now(),LocalDate.now().plusDays(32),"Creating Container", Status.UAT_TEST);
+        ProjectDTO project1 = new ProjectDTO("Spring MVC", "PR001", user1, LocalDate.now(), LocalDate.now().plusDays(25), "Creating Controllers", Status.OPEN);
+        ProjectDTO project2 = new ProjectDTO("Spring ORM", "PR002", user2, LocalDate.now(), LocalDate.now().plusDays(10), "Creating Database", Status.IN_PROGRESS);
+        ProjectDTO project3 = new ProjectDTO("Spring Container", "PR003", user1, LocalDate.now(), LocalDate.now().plusDays(32), "Creating Container", Status.UAT_TEST);
 
         projectService.save(project1);
         projectService.save(project2);
