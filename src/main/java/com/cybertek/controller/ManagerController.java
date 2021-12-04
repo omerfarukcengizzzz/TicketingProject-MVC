2584a61d220b45b7e1a4e7bedfdab811a189f830
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2021-12-04 18:06:11 +0100

    Ticketing Project - MVC

--- src/main/java/com/cybertek/controller/ManagerController.java
+++ src/main/java/com/cybertek/controller/ManagerController.java
@@ -1,9 +1,19 @@
 package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/manager")
 public class ManagerController {
+
+
+    // ----------------- Task Creation -----------------
+    @GetMapping("/task-create")
+    public String createTask() {
+
+        return "/manager/task_assign-create";
+    }
+
 }
