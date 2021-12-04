b2d1e12dbe99ab1aef4a87e70dbf2efd2f321094
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2021-12-04 22:27:41 +0100

    Ticketing Project - MVC

---
+++ src/main/java/com/cybertek/implementation/TaskServiceImpl.java
@@ -0,0 +1,40 @@
+package com.cybertek.implementation;
+
+import com.cybertek.dto.TaskDTO;
+import com.cybertek.service.TaskService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskServiceImpl extends AbstractMapService<TaskDTO, Long> implements TaskService {
+    @Override
+    public TaskDTO save(TaskDTO obj) {
+        return null;
+    }
+
+    @Override
+    public TaskDTO findByID(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<TaskDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public void update(TaskDTO obj) {
+
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+
+    }
+
+    @Override
+    public void delete(TaskDTO obj) {
+
+    }
+}
