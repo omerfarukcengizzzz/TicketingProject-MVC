bd517e4401b32b90fa57f0a746704a8b16fe9b12
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-03-27 00:38:13 +0100

    Ticketing Project - MVC

---
+++ src/main/java/com/cybertek/controller/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.cybertek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/pending-tasks")
+    public String taskStatus() {
+
+
+        return "/employee/pending-tasks";
+    }
+
+}
