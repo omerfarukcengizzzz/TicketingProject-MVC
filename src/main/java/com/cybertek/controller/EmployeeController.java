3a8dad25eb6a8e3e3f0572d602d408b2d9b7d653
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-03-27 01:01:17 +0100

    Ticketing Project - MVC

--- src/main/java/com/cybertek/controller/EmployeeController.java
+++ src/main/java/com/cybertek/controller/EmployeeController.java
@@ -1,6 +1,13 @@
 package com.cybertek.controller;
 
+import com.cybertek.dto.TaskDTO;
+import com.cybertek.enums.Status;
+import com.cybertek.service.ProjectService;
+import com.cybertek.service.TaskService;
+import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +15,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/employee")
 public class EmployeeController {
 
-    @GetMapping("/pending-tasks")
-    public String taskStatus() {
+    //DI
+    @Autowired
+    UserService userService;
+    @Autowired
+    ProjectService projectService;
+    @Autowired
+    TaskService taskService;
 
+    // ----------------- Task Status -----------------
+    @GetMapping("/pending-tasks")
+    public String taskStatus(Model model) {
+
+        model.addAttribute("task", new TaskDTO());
+        model.addAttribute("projectList", projectService.findAll());
+        model.addAttribute("employeeList", userService.findEmployees());
+        model.addAttribute("taskList", taskService.findAll());
+        model.addAttribute("statusList", Status.values());
 
         return "/employee/pending-tasks";
     }
